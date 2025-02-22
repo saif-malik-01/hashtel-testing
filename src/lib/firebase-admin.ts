9f0f09a4a0275dd94adc13c16a832f9d7f3bda50
Saif Malik <saif.malik@mapsily.com>
2025-02-22 13:07:57 +0530

    fix private key

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-      privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: process.env.PRIVATE_KEY?.split(String.raw`\n`).join('\n'),
       clientEmail: process.env.CLIENT_EMAIL,
     }),
     storageBucket: "hashtel-ecom.appspot.com",
